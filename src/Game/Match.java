c91b08aee72a189fc39b927069d67d4314d8be57
rocha <rjoao0112@gmail.com>
2022-11-24 15:32:25 +0000

    Start and Quit Method's creation inside of Match class

--- src/Game/Match.java
+++ src/Game/Match.java
@@ -1,8 +1,30 @@
 package Game;
 
+import java.util.Scanner;
+
 public class Match {
 
     public void start(){
+        Scanner sc = new Scanner();
+        do{
 
+
+
+        }while (quit(sc));
+    }
+
+    private boolean quit(Scanner scanner){
+        String input;
+        do {
+            System.out.println();
+            System.out.println("Do you wanna play again?");
+            System.out.println();
+            System.out.println(" 1 - YES");
+            System.out.println(" 0 - NO");
+            System.out.println();
+            input = scanner.next();
+        }while (!input.equals("1") && !input.equals("0"));
+
+        return input.equals("1");
     }
 }
